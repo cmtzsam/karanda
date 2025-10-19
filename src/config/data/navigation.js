9886be3c7f784cebfdf340a1af748283c79c5b1f
codificarlos <codificarlos@codificarloss-Mac-mini.local>
2025-10-19 12:52:57 -0600

    Featured: add header and footer to the layout

---
+++ src/config/data/navigation.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    text: "Inicio",
+    href: "/",
+  },
+  {
+    text: "Amenidades",
+    href: "/amenidades",
+  },
+  {
+    text: "Departamentos",
+    href: "/departamentos",
+  },
+  {
+    text: "Resenas",
+    href: "/resenas",
+  },
+  {
+    text: "Sobre Tulum",
+    href: "/sobretulum",
+  },
+];
